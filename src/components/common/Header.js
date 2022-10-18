e93822b798707aa05c111af301b2669c0f02d01f
YUJOO-LEE <lllllllllee@gmail.com>
2022-10-18 22:39:43 +0900

    header 로고 index로 링크

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Logo from "./Logo";
 
 export default function Header() {
@@ -5,8 +6,10 @@ export default function Header() {
     <header>
       <div className="inner">
         <h1>
-          <Logo></Logo>
-          <span>청약닷컴</span>
+          <Link to={'/'}>
+            <Logo></Logo>
+            <span>청약닷컴</span>
+          </Link>
         </h1>
       </div>
     </header>
